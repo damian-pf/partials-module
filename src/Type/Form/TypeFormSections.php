39acb1e18d55689fa2b42f5e38799208f2f452ed
Ryan Thompson <service@aiwebsystems.com>
2015-07-01 09:59:18 -0500

    Fix

--- src/Type/Form/TypeFormSections.php
+++ src/Type/Form/TypeFormSections.php
@@ -1,8 +1,21 @@
 <?php namespace Anomaly\PartialsModule\Type\Form;
 
+/**
+ * Class TypeFormSections
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\PartialsModule\Type\Form
+ */
 class TypeFormSections
 {
 
+    /**
+     * Handle the form sections.
+     *
+     * @param TypeFormBuilder $builder
+     */
     public function handle(TypeFormBuilder $builder)
     {
         $builder->setSections(

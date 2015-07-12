f272a24728cab77a64247b5c3262f1f2c9c5f2b7
MarceauKa <marceau@casals.fr>
2015-07-12 18:38:29 +0200

    Ajout de la traduction française

---
+++ resources/lang/fr/button.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'new_partial' => 'Nouveau partial',
+    'new_type'    => 'Nouveau type de partial',
+    'new_field'   => 'Nouveau champs',
+    'add_field'   => 'Ajouter un champs'
+];
---
+++ resources/lang/fr/field.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'name'        => [
+        'name'         => 'Nom',
+        'instructions' => 'Quel est le nom de ce type de partial ?'
+    ],
+    'slug'        => [
+        'name'         => 'Slug',
+        'placeholder'  => 'hello-world',
+        'instructions' => 'Le slug est utilisé pour identifier le type dans l\'URL.'
+    ],
+    'css'         => [
+        'name'         => 'CSS',
+        'instructions' => 'Fichiers CSS supplémentaires. Ils sont compatibles avec les tags.'
+    ],
+    'js'          => [
+        'name'         => 'JS',
+        'instructions' => 'Fichiers JS supplémentaires. Ils sont compatibles avec les tags.'
+    ],
+    'description' => [
+        'name'         => 'Description',
+        'instructions' => 'Description brève du type de partial.'
+    ],
+    'layout'      => [
+        'name'         => 'Template',
+        'instructions' => 'Le template sera utilisé pour afficher le partial.'
+    ]
+];
